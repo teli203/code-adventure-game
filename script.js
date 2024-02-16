73aba8b91cbe3f3d8976b43443101e7c0f844d16
teli203@aol.com <teli203@aol.com>
2024-02-15 19:26:20 -0600

    fixed typo for story to show,change bkgrd color in body

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const storyElement = document.getElementById("story");
 const choicesElement = document.getElementById("choices")
 
 function toggleButtonState(disable) {                   /* disable the btns when txt is loading so it doesn't jump ahead */
-    const buttons = document.querySelectorAll("./choice");
+    const buttons = document.querySelectorAll(".choice");
     for (let button of buttons) {
         button.disable = disable;
     }
