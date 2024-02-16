461c334f804bbfca5f194057e032ddb96953f3b7
teli203 <teli203@aol.com>
2024-02-15 23:03:09 -0600

    fixed disabled btn 4 txt load

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const choicesElement = document.getElementById("choices")
 function toggleButtonState(disable) {                   /* disable the btns when txt is loading so it doesn't jump ahead */
     const buttons = document.querySelectorAll(".choice");
     for (let button of buttons) {
-        button.disable = disable;
+        button.disabled = disable;
     }
 }
 
